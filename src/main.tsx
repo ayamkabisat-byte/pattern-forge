b715af0e01de9ead41c3fc1a91b0c2c955b84e36
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-22 15:35:46 +0700

    chore: switch app entry to v0.6

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV05'
+import App from './AppV06'
 import './styles.css'
 import './v05.css'
+import './v06.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
