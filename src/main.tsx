b22fa500d5199d15532137d3de00e1b45515e48a
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-22 15:48:21 +0700

    chore: switch entry to direct final canvas composer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV06b'
+import App from './AppV061'
 import './styles.css'
 import './v05.css'
 import './v06.css'
+import './v061.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
