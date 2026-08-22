90d1f8fe42733d564a4c185c102fcc1f03560b9d
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-22 18:45:05 +0700

    v0.7 switch app entry to multilingual typography build

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV062'
+import App from './AppV07'
 import './styles.css'
 import './v05.css'
 import './v06.css'
 import './v061.css'
 import './v062.css'
+import './v07.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
