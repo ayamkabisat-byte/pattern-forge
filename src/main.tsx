49dbe09cf9518c691cb82c2890b9406196e5d47b
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-22 15:16:09 +0700

    style: load v0.5 controls

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppV05'
 import './styles.css'
+import './v05.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
