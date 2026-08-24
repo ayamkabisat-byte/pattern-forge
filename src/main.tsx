4d7cc4120fa3d2d5a4779b0051d8b28d0332c664
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-25 00:14:02 +0700

    Switch PatternForge to v0.9 freeform composer

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV08'
+import App from './AppV09'
 import './styles.css'
 import './v05.css'
 import './v06.css'
 import './v061.css'
 import './v062.css'
 import './v08.css'
+import './v09.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
