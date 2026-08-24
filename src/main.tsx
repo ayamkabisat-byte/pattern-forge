09f715f4062ca6d5ca8b1c7aad29b0e570f1fd92
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-24 23:55:09 +0700

    v0.8 make seamless pattern system the production app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV07'
+import App from './AppV08'
 import './styles.css'
 import './v05.css'
 import './v06.css'
 import './v061.css'
 import './v062.css'
-import './v07.css'
+import './v08.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
