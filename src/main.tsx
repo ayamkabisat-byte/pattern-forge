c798cb0d50bef88843810b893ca865adbb13aa8d
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-25 23:34:08 +0700

    Switch production entry to PatternForge v1 preview workspaces

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV09'
+import App from './AppV10'
 import './styles.css'
 import './v05.css'
 import './v06.css'
@@ -9,6 +9,7 @@ import './v062.css'
 import './v08.css'
 import './v09.css'
 import './v091.css'
+import './v10.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
